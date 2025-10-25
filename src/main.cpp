2eedcd798c5e7040d8d174989e74fe8a3a486239
Rishav Kalani <rishavimrk@gmail.com>
2025-10-25 17:03:41 +0530

    new

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <unistd.h>
 using namespace std;
+
+string findExecutableInPath(const string &command)
+{
+  char *pathenv = getenv("PATH");
+  if (!pathenv)
+  {
+    return "";
+  }
+  string pathStr = pathenv;
+  stringstream ss(pathStr);
+  // constrcuting a stringstream of the path
+  string dir;
+  while (getline(ss, dir, ';'))
+  {
+    string fullPath = dir + "\\" + command;
+    // constructed the fullpath for the file
+    if (_access(fullPath.c_str(), 0) == 0) // checking the access of the file
+    {
+      // c_str() converts the string into a char pointer so that i can give access to the file location
+      return fullPath;
+    }
+  }
+  return "";
+}
+
 int main()
 {
   // Flush after every std::cout / std:cerr
@@ -25,13 +52,20 @@ int main()
     }
     else if (cmd.size() >= 4 && cmd.substr(0, 4) == "type")
     {
-      if (cmd.substr(5) == "echo" || cmd.substr(5) == "type" || cmd.substr(5) == "exit")
+      string arg = cmd.substr(5);
+      if (cmd.substr(5) == "echo" || cmd.substr(5) == "exit" || cmd.substr(5) == "type")
       {
         cout << cmd.substr(5) << " is a shell builtin" << endl;
       }
       else
       {
-        cout << cmd.substr(5) << ": not found" << endl;
+        string path = findExecutableInPath(arg);
+        if (!path.empty())
+        {
+          cout << cmd << "is" << path << endl;
+        }
+        else
+          cout << arg << ": not found" << endl;
       }
     }
     else
