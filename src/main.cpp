2fa7653be5f558954c767d44120a3b6cd2c65b73
Rishav Kalani <rishavimrk@gmail.com>
2026-01-06 00:42:09 +0530

    ritwik loves anuj panty

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,14 @@ int main() {
   // Flush after every std::cout / std:cerr
   cout << unitbuf;
   cerr << unitbuf;
-  cout << "$ ";
-  string cmd;
-  getline(cin , cmd);
-  cout << cmd << ": command not found" << endl;
+  while(true)
+  {
+    cout << "$ ";
+    string cmd;
+    getline(cin , cmd);
+    cout << cmd << ": command not found" << endl;
+    
+  }
+  
   // TODO: Uncomment the code below to pass the first stage
 }
