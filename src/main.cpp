f09278076f42fcf696c33081af3b53dfab3a7b58
Rishav Kalani <rishavimrk@gmail.com>
2026-01-06 21:27:32 +0530

    new commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <sstream>
 #include <unistd.h>
+#include <vector>
+#include <sys/wait.h>
 using namespace std;
 
 string findExecutableInPath(const string &command){
@@ -67,8 +69,41 @@ int main() {
       cout << cmd.substr(5) << endl;
     }
     else{
-      cout << cmd << ": command not found" << endl;
+      vector<string> words;
+      stringstream ss(cmd);
+      string dir;
+      while(ss >> dir)
+      {
+        words.push_back(dir);
+      }
+      string s=words[0];
+      string execpath=findExecutableInPath(s);
+      if(execpath.empty())
+      {
+        cout << words[0] << ": command not found" << endl;
+        continue;
+      }
+      vector<char *> argv;
+      for(auto &w:words){
+        argv.push_back(const_cast<char *>(w.c_str()));
+      }
+      argv.push_back(nullptr);
+      pid_t pid=fork();
+      if(pid==0)
+      {
+        //child process
+        execv(execpath.c_str(),argv.data());
+        perror("execv");
+        _exit(1);
+      }
+      else{
+        //parent process
+        int status;
+        waitpid(pid,&status,0);
+        //waits for the status update from child.
+      }
     } 
+    return 0;
   }
   
   // TODO: Uncomment the code below to pass the first stage
