7af54aed2a3cd05761c666fbd303872bc52c6afa
Rishav Kalani <rishavimrk@gmail.com>
2026-01-06 18:46:02 +0530

    new commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <unistd.h>
 using namespace std;
+
+string findExecutableInPath(const string &command){
+  if(command.find('/')!=string::npos)
+  {
+    if(access(command.c_str(),X_OK)==0)
+    {
+      return command;
+    }
+    return "";
+  }
+  char *pathenv=getenv("PATH");
+  if(!pathenv)
+  {
+    return "";
+  }
+  string pathStr=pathenv;
+  stringstream ss(pathStr);
+  string dir;
+  while(getline(ss,dir,':'))
+  {
+    string fullpath=dir+"/"+command;
+    if(access(fullpath.c_str(),X_OK)==0)
+    {
+      return fullpath;
+    }
+  }
+  return "";
+}
 int main() {
   // Flush after every std::cout / std:cerr
   cout << unitbuf;
@@ -16,12 +46,20 @@ int main() {
     }
     else if(cmd.size()>=4 && cmd.substr(0,4)=="type")
     {
+      string arg=cmd.substr(5);
       if(cmd.substr(5)=="echo" || cmd.substr(5)=="exit" || cmd.substr(5)=="type")
       {
         cout << cmd.substr(5) << " is a shell builtin" << endl;
       }
       else{
-        cout << cmd.substr(5) << ": not found" << endl;
+        string path=findExecutableInPath(arg);
+        if(!path.empty())
+        {
+          cout << arg << " is " << path << endl;
+        }
+        else{
+          cout << cmd.substr(5) << ": not found" << endl;
+        }
       }
     }
     else if(cmd.size()>=4 && cmd.substr(0,4)=="echo")
